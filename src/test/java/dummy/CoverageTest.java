f3638025ad5c5f805f774e649d6e0999668bb2f1
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-12-04 10:54:47 +0100

    build: test coverage please work

---
+++ src/test/java/dummy/CoverageTest.java
@@ -0,0 +1,14 @@
+package dummy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoverageTest {
+
+    @Test
+    void coverageTest() {
+        Coverage coverage = new Coverage();
+    }
+
+}
