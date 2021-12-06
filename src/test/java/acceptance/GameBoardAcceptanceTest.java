df448887fcb9121531827389366854c9e3ae54af
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-12-07 00:43:30 +0100

    fix: 2x2 board - GREEN - Renamed Acceptance test

--- src/test/java/acceptance/GameBoardAcceptanceTest.java
+++ src/test/java/acceptance/GameBoardAcceptanceTest.java
@@ -11,7 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith({ MockitoExtension.class })
-class ShowGameBoardAcceptanceTest {
+class GameBoardAcceptanceTest {
 
     private static final String EMPTY_BOARD = BoardTestHelper.createEmptyBoard();
 
